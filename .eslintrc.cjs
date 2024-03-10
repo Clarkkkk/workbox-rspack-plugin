b63b5e849c45ac321dc131ac06c7669e5639398c
Aaron_Zhou <clark1729@outlook.com>
2024-03-10 15:10:56 +0800

    fix: fix test errors caused by `rollup-plugin-node-externals`

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,7 +12,7 @@ module.exports = {
     },
     plugins: ['@typescript-eslint', 'simple-import-sort'],
     rules: {
-        indent: ['error', 4, { SwitchCase: 1 }],
+        indent: 'off',
         'no-console': ['warn', { allow: ['warn', 'error'] }],
         'consistent-return': 'error',
         'space-before-function-paren': [
--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,29 @@ import { nodeExternals } from 'rollup-plugin-node-externals'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
-export default defineConfig({
-    base: './',
-    assetsInclude: ['**/*.node'],
-    build: {
-        lib: {
-            entry: {
-                index: path.resolve(__dirname, 'src/index.ts')
-            },
-            formats: ['cjs', 'es']
-        }
-    },
-    plugins: [
-        dts(),
-        {
-            ...nodeExternals({
-                deps: false
-            }),
-            enforce: 'pre'
-        }
-    ],
-    test: {}
+export default defineConfig(({ mode }) => {
+    return {
+        base: './',
+        assetsInclude: ['**/*.node'],
+        build: {
+            lib: {
+                entry: {
+                    index: path.resolve(__dirname, 'src/index.ts')
+                },
+                formats: ['cjs', 'es']
+            }
+        },
+        plugins:
+            mode === 'test'
+                ? []
+                : [
+                      dts(),
+                      {
+                          ...nodeExternals({
+                              deps: false
+                          }),
+                          enforce: 'pre'
+                      }
+                  ]
+    }
 })
